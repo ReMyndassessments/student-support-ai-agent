bf8505ae574a6d0988537bf5e0526ffa50ac9c98
Leap <git@leap.new>
2025-08-17 15:06:01 +0000

    Configure Clerk authentication with publishable key

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,3 @@
 // The Clerk publishable key, to initialize Clerk.
 // Leave empty to disable authentication and run in demo mode
-export const clerkPublishableKey = "";
+export const clerkPublishableKey = "pk_test_c2hpbmluZy13aGlwcGV0LTgwLmNsZXJrLmFjY291bnRzLmRldiQ";
